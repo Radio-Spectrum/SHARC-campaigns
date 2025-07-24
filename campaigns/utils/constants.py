afdd969c1f2970b34ed6516c9f3683e1349494c7
artistrea <apiravesi@gmail.com>
2025-07-24 00:19:18 -0300

    update(utils): sharc root constant

--- campaigns/utils/constants.py
+++ campaigns/utils/constants.py
@@ -3,7 +3,7 @@ import sharc
 
 ROOT_DIR = Path(__file__).parents[2].absolute()
 ROOT_PARAMS_DIR = ROOT_DIR / "from-docs"
-SHARC_SIM_ROOT_DIR = Path(sharc.__file__).parents[1]
+SHARC_SIM_ROOT_DIR = Path(sharc.__file__).parents[0]
 
 if __name__ == "__main__":
     print("ROOT_DIR", ROOT_DIR)
