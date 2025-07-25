e57ddda1d429e9a216398a483db5a65b34340010
artistrea <apiravesi@gmail.com>
2025-07-25 16:24:48 -0300

    update(campaigns): mss_d2d_to_imt_cross_border better scripts

---
+++ campaigns/mss_d2d_to_imt_cross_border/generate_params.py
@@ -0,0 +1,155 @@
+from campaigns.utils.parameters_factory import ParametersFactory
+from campaigns.utils.dump_parameters import dump_parameters
+from campaigns.mss_d2d_to_imt_cross_border.run import CAMPAIGN_STR, CAMPAIGN_DIR, get_output_dir_start, INPUTS_DIR
+
+from campaigns.mss_d2d_to_imt_cross_border.cmd_parser import get_cmd_parser
+
+from sharc.antenna.antenna_s1528 import AntennaS1528Taylor
+
+import numpy as np
+
+def generate(
+    num_snapshots: int,
+    mss_id: str,
+    co_channel: bool,
+):
+    print(f"Generating inputs for {mss_id}, co_channel={co_channel}")
+    general = {
+        "seed": 101,
+        ###########################################################################
+        # Number of simulation snapshots
+        ###########################################################################
+        "num_snapshots": num_snapshots,
+        "overwrite_output": False,
+        "system": "MSS_D2D",
+    }
+
+    factory = ParametersFactory()
+
+    params = factory.load_from_id(
+            "imt.1-3GHz.single-bs.aas-macro-bs"
+        ).load_from_id(
+            mss_id
+        ).load_from_dict(
+            {"general": general}
+        ).build()
+
+    #####
+    # scenario
+    params.imt.interfered_with = True
+
+    params.imt.frequency = 2160.0
+    params.mss_d2d.frequency = 2160.0
+
+    if not co_channel:
+        params.mss_d2d.frequency += (
+            params.imt.bandwidth + params.mss_d2d.bandwidth
+        ) / 2
+
+    params.general.enable_cochannel = co_channel
+    params.general.enable_adjacent_channel = True
+
+    params.mss_d2d.adjacent_ch_emissions = "SPECTRAL_MASK"
+    params.imt.adjacent_ch_reception = "OFF"
+
+    #######
+    # imt parameters
+    # International Friendship Bridge
+    params.imt.topology.central_latitude = -25.5549751
+    params.imt.topology.central_longitude = -54.5746686
+    params.imt.topology.central_altitude = 200
+
+    #######
+    # mss parameters
+
+    # Beam pointing
+    params.mss_d2d.beam_positioning.type = "SERVICE_GRID"
+    params.mss_d2d.beam_positioning.service_grid.country_names = ["Brazil", "Argentina"]
+    # this is distance in km so that actual best satellite is used for each grid point
+    angle_dist_between_planes = 360 / params.mss_d2d.orbits[0].n_planes
+    margin = -np.ceil((angle_dist_between_planes / 2) * 111)
+    params.mss_d2d.beam_positioning.service_grid.eligible_sats_margin_from_border = int(margin)
+
+    # Beam is active if
+    params.mss_d2d.sat_is_active_if.conditions = [
+        "LAT_LONG_INSIDE_COUNTRY",
+        "MINIMUM_ELEVATION_FROM_ES",
+    ]
+    params.mss_d2d.sat_is_active_if.lat_long_inside_country.country_names = ["Brazil", "Argentina"]
+    params.mss_d2d.sat_is_active_if.lat_long_inside_country.margin_from_border = \
+        params.mss_d2d.beam_positioning.service_grid.eligible_sats_margin_from_border
+
+    # Get cell radius
+    params.mss_d2d.antenna_s1528.frequency = params.mss_d2d.frequency
+    antenna = AntennaS1528Taylor(
+        params.mss_d2d.antenna_s1528
+    )
+    off_axis = np.linspace(0, 20, int(1e6))
+    gains = antenna.calculate_gain(
+        off_axis_angle_vec=off_axis,
+        theta_vec=0,
+    )
+    angle_7dB_i = np.where(gains <= params.mss_d2d.antenna_s1528.antenna_gain - 7)[0][0]
+    angle_7dB = off_axis[angle_7dB_i]
+    cell_radius = np.tan(np.deg2rad(angle_7dB)) * params.mss_d2d.orbits[0].apogee_alt_km * 1e3
+
+    # apprx. 36675.5
+    params.mss_d2d.cell_radius = int(cell_radius)
+    print("Calculated cell radius: ", params.mss_d2d.cell_radius)
+
+    # distances = np.linspace(params.mss_d2d.cell_radius / 1e3, 100, 4)
+    # distances = [float(round(d, 3)) for d in distances]
+    distances = [100.000]
+    print("Scenarios of grid border as ", distances)
+    for load in [0.2]:#, 0.5]:
+        params.mss_d2d.beams_load_factor = load
+
+        for link in ["dl", "ul"]:
+            params.general.imt_link = "DOWNLINK" if link == "dl" else "UPLINK"
+
+            for border in distances:
+                params.mss_d2d.beam_positioning.service_grid.grid_margin_from_border = border
+                output_start = get_output_dir_start(mss_id, co_channel)
+                params.general.output_dir = f"{CAMPAIGN_STR}/{output_start}_{link}/"
+
+                postfix = f"mss_d2d_to_imt_cross_border_{border}km_{load}load_{link}"
+                params.general.output_dir_prefix = f"output_{postfix}"
+                file = INPUTS_DIR / f"parameter_{mss_id}_{"co" if co_channel else "adj"}_{postfix}.yaml"
+
+                # Create parent directories if they don't exist
+                dump_parameters(
+                    file, params
+                )
+
+if __name__ == "__main__":
+    parser = get_cmd_parser()
+    parser.add_argument(
+        "--num-of-drops", "-n",
+        type=int,
+        default=int(1e5),
+        help="Number of drops to be simulated"
+    )
+    parser.add_argument(
+        "--dont-clear",
+        action="store_true",
+        help="Mark true if you wish to NOT remove previous parameter "
+            "files in the directory (true/false). Default: false",
+    )
+    args = parser.parse_args()
+
+    INPUTS_DIR.mkdir(parents=True, exist_ok=True)
+
+    if not args.dont_clear:
+        # removes all current inputs in directory
+        for item in INPUTS_DIR.iterdir():
+            if item.is_file() and item.name.endswith(".yaml"):
+                item.unlink()
+
+    print(f"Outputting input files to {CAMPAIGN_DIR}")
+
+    for selected_sys in args.mss:
+        generate(
+            args.num_of_drops,
+            selected_sys,
+            not args.adj,
+        )
--- campaigns/mss_d2d_to_imt_cross_border/plot_results.py
+++ campaigns/mss_d2d_to_imt_cross_border/plot_results.py
@@ -1,6 +1,6 @@
 """Script to process and plot results for MSS D2D to IMT cross-border campaign."""
 
-import os
+import re
 from pathlib import Path
 import plotly.graph_objects as go
 from sharc.results import Results
@@ -12,169 +12,179 @@ from campaigns.utils.constants import SHARC_SIM_ROOT_DIR
 
 OUTPUT_ROOT_FOLDER = SHARC_SIM_ROOT_DIR / CAMPAIGN_STR
 
-post_processor = PostProcessor()
+if __name__ == "__main__":
+    post_processor = PostProcessor()
 
-parser = get_cmd_parser()
-args = parser.parse_args()
+    parser = get_cmd_parser()
+    args = parser.parse_args()
 
-distances_map = {
-    "system-3.2110-2200MHz.525km": [36.726, 57.817, 78.909, 100.0],
-    "system-3.2110-2200MHz.340km": [48.438, 65.625, 82.813, 100.0],
-    "system-4.2110-2200MHz.690km": [23.784, 49.189, 74.595, 100.0],
-}
-if len(args.mss) > 1:
-    raise ValueError("fiz funfar para um sistema so por enquanto")
+    if len(args.mss) > 1:
+        raise ValueError("fiz funfar para um sistema so por enquanto")
 
-mss_id = args.mss[0]
-distances = distances_map[mss_id]
+    mss_id = args.mss[0]
 
-print("Plotting scenarios of grid border as ", distances)
-for load in [0.2, 0.5]:
-    for link in ["dl", "ul"]:
-        for border in distances:
-            out_prefix = f"output_mss_d2d_to_imt_cross_border_{border}km_{load}load_{link}"
-            post_processor\
-                .add_plot_legend_pattern(
-                    dir_name_contains=out_prefix,
-                    legend=f"{border}km border; {int(load*100)}% load"
-                )
+    def legend_gen(dirname):
+        print(dirname)
+        pattern = re.compile(
+            r"output_mss_d2d_to_imt_cross_border_(\d+\.\d+)km_(\d+\.\d+)load_([ud]l)_"
+        )
+        match = pattern.match(dirname)
+        if not match:
+            return "Unknown"        
+        
+        border_km, load_pct, link_type = match.groups()
 
-prefixes = [f"{d}km" for d in distances]
-styles = ["solid", "dot", "dash", "longdash", "dashdot", "longdashdot"]
+        link_type = link_type.upper()
+        load_pct = float(load_pct) * 100
 
-def linestyle_getter(result: Results):
-    """
-    Returns a line style string based on the prefix found in the result's output directory.
-    """
-    for i in range(len(prefixes)):
-        if "_" + prefixes[i] in result.output_directory:
-            return styles[i]
-    return "solid"
-post_processor.add_results_linestyle_getter(linestyle_getter)
+        return f"{load_pct}% load, {border_km} km border, IMT TN {link_type}"
 
-campaign_base_dir = str((Path(__file__) / ".." / "..").resolve())
+    post_processor.add_plot_legend_generator(legend_gen)
 
-attributes_to_plot = [
-    "imt_system_antenna_gain",
-    "system_imt_antenna_gain",
-    "sys_to_imt_coupling_loss",
-    "imt_system_path_loss",
-    "imt_dl_pfd_external",
-    "imt_dl_pfd_external_aggregated",
-    "imt_dl_inr",
-    "imt_ul_inr"
-]
+    styles = ["solid", "dot", "dash", "longdash", "dashdot", "longdashdot"]
 
-output_start = get_output_dir_start(mss_id, not args.adj)
+    def linestyle_getter(result: Results):
+        """
+        Returns a line style string based on the prefix found in the result's output directory.
+        """
+        dirname = result.output_directory
+        pattern = re.compile(
+            r"output_mss_d2d_to_imt_cross_border_(\d+)km_(\d+)load_(.+)"
+        )
+        match = pattern.match(dirname)
+        if not match:
+            return "solid"
 
-results_dl = Results.load_many_from_dir(
-    OUTPUT_ROOT_FOLDER / f"{output_start}_dl",
-    only_latest=True,
-    only_samples=attributes_to_plot
-)
-# print("len(results_dl)", len(results_dl))
-# for i in range(len(results_dl)):
-#     print(results_dl[i].imt_dl_inr)
-# exit()
-results_ul = Results.load_many_from_dir(
-    OUTPUT_ROOT_FOLDER / f"{output_start}_ul",
-    only_latest=True,
-    only_samples=attributes_to_plot
-)
-# print("len(results_ul)", len(results_ul))
-# ^: typing.List[Results]
-all_results = [*results_ul, *results_dl]
+        border_km, load_pct, link_type = match.groups()
 
-# If set to True the plots will be opened in the browser automatically
-auto_open = False
+        link_type = link_type.uppercase()
+        load_pct = float(load_pct) * 100
+        if load_pct == "0.2":
+            return "dot"
+        return "solid"
 
-post_processor.add_results(all_results)
+    post_processor.add_results_linestyle_getter(linestyle_getter)
 
-plots = post_processor.generate_ccdf_plots_from_results(
-    all_results
-)
-post_processor.add_plots(plots)
+    campaign_base_dir = str((Path(__file__) / ".." / "..").resolve())
 
-# Add a protection criteria line:
-protection_criteria = -6
-perc_of_time = 0.01
+    attributes_to_plot = [
+        "imt_system_antenna_gain",
+        "system_imt_antenna_gain",
+        "sys_to_imt_coupling_loss",
+        "imt_system_path_loss",
+        "imt_dl_pfd_external",
+        "imt_dl_pfd_external_aggregated",
+        "imt_dl_inr",
+        "imt_ul_inr"
+    ]
 
-for attr in ["imt_dl_inr", "imt_ul_inr"]:
-    plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type='ccdf')
-    if plot is not None:
-        plot.add_vline(protection_criteria, line_dash="dash", annotation=dict(
-            text="Protection criteria",
-            xref="x",
-            yref="paper",
-            x=protection_criteria + 1.0,  # Offset for visibility
-            y=0.95
-        ))
-        plot.add_hline(perc_of_time, line_dash="dash")
-    else:
-        print(f"Warning: No plot found for attribute '{attr}'")
+    output_start = get_output_dir_start(mss_id, not args.adj)
 
-pfd_protection_criteria = -109
-for attr in ["imt_dl_pfd_external", "imt_dl_pfd_external_aggregated"]:
-    plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type='ccdf')
-    if plot is not None:
-        plot.add_vline(pfd_protection_criteria, line_dash="dash", annotation=dict(
-            text="PFD protection criteria",
-            xref="x",
-            yref="paper",
-            x=pfd_protection_criteria + 1.0,  # Offset for visibility
-            y=0.95
-        ))
-    else:
-        print(f"Warning: No plot found for attribute '{attr}'")
+    results_dl = Results.load_many_from_dir(
+        OUTPUT_ROOT_FOLDER / f"{output_start}_dl",
+        only_latest=True,
+        only_samples=attributes_to_plot
+    )
 
-# for attr in attributes_to_plot:
-#     post_processor.get_plot_by_results_attribute_name(attr).show()
+    results_ul = Results.load_many_from_dir(
+        OUTPUT_ROOT_FOLDER / f"{output_start}_ul",
+        only_latest=True,
+        only_samples=attributes_to_plot
+    )
+    # print("len(results_ul)", len(results_ul))
+    # ^: typing.List[Results]
+    all_results = [*results_ul, *results_dl]
 
-# Ensure the "htmls" directory exists relative to the script directory
-htmls_dir = OUTPUT_ROOT_FOLDER / "htmls"
-htmls_dir.mkdir(exist_ok=True)
-selected_str = output_start
-specific_dir = htmls_dir / selected_str
-specific_dir.mkdir(exist_ok=True)
+    # If set to True the plots will be opened in the browser automatically
+    auto_open = False
 
-for attr in attributes_to_plot:
-    plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type="ccdf")
-    if plot is None:
-        print(f"Warning: No plot found for attribute '{attr}'")
-        continue
-    plot.write_html(specific_dir / f"{attr}.html")
-    # plot.show()
+    post_processor.add_results(all_results)
+
+    plots = post_processor.generate_ccdf_plots_from_results(
+        all_results
+    )
+    post_processor.add_plots(plots)
+
+    # Add a protection criteria line:
+    protection_criteria = -6
+    perc_of_time = 0.01
+
+    for attr in ["imt_dl_inr", "imt_ul_inr"]:
+        plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type='ccdf')
+        if plot is not None:
+            plot.add_vline(protection_criteria, line_dash="dash", annotation=dict(
+                text="Protection criteria",
+                xref="x",
+                yref="paper",
+                x=protection_criteria + 1.0,  # Offset for visibility
+                y=0.95
+            ))
+            plot.add_hline(perc_of_time, line_dash="dash")
+        else:
+            print(f"Warning: No plot found for attribute '{attr}'")
+
+    pfd_protection_criteria = -109
+    for attr in ["imt_dl_pfd_external", "imt_dl_pfd_external_aggregated"]:
+        plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type='ccdf')
+        if plot is not None:
+            plot.add_vline(pfd_protection_criteria, line_dash="dash", annotation=dict(
+                text="PFD protection criteria",
+                xref="x",
+                yref="paper",
+                x=pfd_protection_criteria + 1.0,  # Offset for visibility
+                y=0.95
+            ))
+        else:
+            print(f"Warning: No plot found for attribute '{attr}'")
+
+    # for attr in attributes_to_plot:
+    #     post_processor.get_plot_by_results_attribute_name(attr).show()
+
+    # Ensure the "htmls" directory exists relative to the script directory
+    htmls_dir = OUTPUT_ROOT_FOLDER / "htmls"
+    htmls_dir.mkdir(exist_ok=True)
+    selected_str = output_start
+    specific_dir = htmls_dir / selected_str
+    specific_dir.mkdir(exist_ok=True)
+
+    for attr in attributes_to_plot:
+        plot = post_processor.get_plot_by_results_attribute_name(attr, plot_type="ccdf")
+        if plot is None:
+            print(f"Warning: No plot found for attribute '{attr}'")
+            continue
+        # plot.write_html(specific_dir / f"{attr}.html")
+        plot.show()
 
 
-# # Now let's plot the beam per satellite results.
-# # We do it manually as PostProcessor does not support histograms yet.
-# # it doesn't matter from where get the results for that statistic.
-# results_num_beams_per_satellite = Results.load_many_from_dir(
-#     os.path.join(campaign_base_dir, f"{output_start}_ul"),
-#     only_latest=True,
-#     only_samples=["mss_d2d_num_beams_per_satellite"],
-#     filter_fn=(lambda dir_name: "output_mss_d2d_to_imt_cross_border_0km_service_grid_padded_50p" in dir_name)
-# )
+    # # Now let's plot the beam per satellite results.
+    # # We do it manually as PostProcessor does not support histograms yet.
+    # # it doesn't matter from where get the results for that statistic.
+    # results_num_beams_per_satellite = Results.load_many_from_dir(
+    #     os.path.join(campaign_base_dir, f"{output_start}_ul"),
+    #     only_latest=True,
+    #     only_samples=["mss_d2d_num_beams_per_satellite"],
+    #     filter_fn=(lambda dir_name: "output_mss_d2d_to_imt_cross_border_0km_service_grid_padded_50p" in dir_name)
+    # )
 
-# mss_d2d_num_beams_per_satellite_attr = getattr(results_num_beams_per_satellite[0],
-#                                                "mss_d2d_num_beams_per_satellite", None)
+    # mss_d2d_num_beams_per_satellite_attr = getattr(results_num_beams_per_satellite[0],
+    #                                                "mss_d2d_num_beams_per_satellite", None)
 
-# if results_num_beams_per_satellite is not None:
-#     fig = go.Figure()
-#     fig.add_trace(go.Histogram(
-#         x=mss_d2d_num_beams_per_satellite_attr,
-#         nbinsx=15,
-#         histnorm='probability',
-#         marker_color='blue',
-#         opacity=0.75
-#     ))
-#     fig.update_layout(
-#         title="Number of Beams per Satellite Histogram",
-#         xaxis_title="Number of Beams per Satellite",
-#         yaxis_title="Frequency",
-#         # template="plotly_white",
-#         bargap=0.2,
-#     )
-#     fig.write_html(specific_dir / "mss_d2d_num_beams_per_satellite_hist.html")
-#     if auto_open:
-#         fig.show()
+    # if results_num_beams_per_satellite is not None:
+    #     fig = go.Figure()
+    #     fig.add_trace(go.Histogram(
+    #         x=mss_d2d_num_beams_per_satellite_attr,
+    #         nbinsx=15,
+    #         histnorm='probability',
+    #         marker_color='blue',
+    #         opacity=0.75
+    #     ))
+    #     fig.update_layout(
+    #         title="Number of Beams per Satellite Histogram",
+    #         xaxis_title="Number of Beams per Satellite",
+    #         yaxis_title="Frequency",
+    #         # template="plotly_white",
+    #         bargap=0.2,
+    #     )
+    #     fig.write_html(specific_dir / "mss_d2d_num_beams_per_satellite_hist.html")
+    #     if auto_open:
+    #         fig.show()
