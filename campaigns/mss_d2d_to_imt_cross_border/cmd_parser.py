3bba0ea2a1a1a6e380c8718475abbe04f6499374
Bruno Faria <brunohcfaria@gmail.com>
2025-09-24 17:02:02 -0300

    update(campaign): Added reference to new DC-MSS system 2300-2690MHz

--- campaigns/mss_d2d_to_imt_cross_border/cmd_parser.py
+++ campaigns/mss_d2d_to_imt_cross_border/cmd_parser.py
@@ -3,11 +3,13 @@ import argparse
 OPTION_TO_SELECTED_SYS = {
     "3.1": ["system-3.2110-2200MHz.525km"],
     "3.2": ["system-3.2110-2200MHz.340km"],
+    "3.3": ["system-3.2300-2690MHz.525km"],
     # "4": ["system-4.2110-2200MHz.690km"],
     "all": [
         "system-3.2110-2200MHz.525km",
         "system-3.2110-2200MHz.340km",
         # "system-4.2110-2200MHz.690km",
+        "system-3.2300-2690MHz.525km"
     ]
 }
 
