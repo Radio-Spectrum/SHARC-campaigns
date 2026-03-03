394ee798f1c67992b923b0dba5dce32d28511b80
rdvieira <robson.vieira@unb.br>
2026-03-03 16:40:03 -0300

    ARNS scenarios

--- campaigns/mss_d2d_to_arns_dme_study/generate_inputs.py
+++ campaigns/mss_d2d_to_arns_dme_study/generate_inputs.py
@@ -156,17 +156,17 @@ def generate_inputs():
 
         #################### MSS Earth Station Parameters ##############
         # MHz. Setting to the center of the victim ES band (Hibleo-X) to be more conservative.
-        params.single_earth_station.frequency = 964.5 - params.single_earth_station.bandwidth / 2
+        params.single_earth_station.frequency = 962 + params.single_earth_station.bandwidth / 2
         # position ES at reference
         es_geom = params.single_earth_station.geometry
         es_geom.height = 10000  # meters
         # Let the MSS Earth station be randomly located within the service area.
-        es_geom.location.type = "NETWORK"
+        es_geom.location.type = "UNIFORM_DIST"
         # These coordinates are relative to the topology central longitude - Asunción in this case.
-        # es_geom.location.uniform_dist.min_dist_to_center = 1e-2  # make it small - close to center
-        # es_geom.location.uniform_dist.max_dist_to_center = 1000e3
-        es_geom.location.network.min_dist_to_bs = params.imt.topology.mss_dc.beam_radius
-
+         es_geom.location.uniform_dist.min_dist_to_center = 1e-2  # make it small - close to center
+         es_geom.location.uniform_dist.max_dist_to_center = 1000e3
+    
+        0
         # Vary antenna pointinhg angles uniformly.
         es_geom.azimuth.type = "UNIFORM_DIST"
         es_geom.azimuth.uniform_dist.max = 180.
--- campaigns/mss_d2d_to_arns_radar_study/generate_inputs.py
+++ campaigns/mss_d2d_to_arns_radar_study/generate_inputs.py
@@ -161,11 +161,11 @@ def generate_inputs():
         es_geom = params.single_earth_station.geometry
         es_geom.height = 1.5  # meters
         # Let the MSS Earth station be randomly located within the service area.
-        es_geom.location.type = "NETWORK"
+        es_geom.location.type = "UNIFORM_DIST"
         # These coordinates are relative to the topology central longitude - Asunción in this case.
-        # es_geom.location.uniform_dist.min_dist_to_center = 1e-2  # make it small - close to center
-        # es_geom.location.uniform_dist.max_dist_to_center = 1000e3
-        es_geom.location.network.min_dist_to_bs = params.imt.topology.mss_dc.beam_radius
+        es_geom.location.uniform_dist.min_dist_to_center = 1e-2  # make it small - close to center
+        es_geom.location.uniform_dist.max_dist_to_center = 1000e3
+        #es_geom.location.network.min_dist_to_bs = params.imt.topology.mss_dc.beam_radius
 
         # Vary antenna pointinhg angles uniformly.
         es_geom.azimuth.type = "UNIFORM_DIST"
